2c6aa3e5f2458a01c65001b0e1e9943bd497904f
Mateo Castano <m.castano1@uniandes.edu.co>
2024-03-04 10:40:42 -0500

    :construction: Adding router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-
-
+import { SideBarComponent } from './pages/side-bar/side-bar.component';
 
 @NgModule({
-  declarations: [
-    HomePageComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomePageComponent, AboutPageComponent, SideBarComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HomePageComponent, AboutPageComponent, SideBarComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
