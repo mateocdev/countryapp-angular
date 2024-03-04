76eed0ae6dfeb2ff3260090dbb0169a41a598b74
Mateo Castano <m.castano1@uniandes.edu.co>
2024-03-04 10:59:23 -0500

    :construction: Adding routing module to countries

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,23 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SideBarComponent } from './pages/side-bar/side-bar.component';
 
 @NgModule({
-  declarations: [HomePageComponent, AboutPageComponent, SideBarComponent],
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    SideBarComponent,
+    ContactPageComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [HomePageComponent, AboutPageComponent, SideBarComponent],
+  exports: [
+    HomePageComponent,
+    AboutPageComponent,
+    SideBarComponent,
+    ContactPageComponent,
+  ],
 })
 export class SharedModule {}
