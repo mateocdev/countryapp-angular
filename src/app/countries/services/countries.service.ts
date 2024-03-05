21a205233a0d44735fefdaa7296f84292258a455
Mateo Castano <m.castano1@uniandes.edu.co>
2024-03-05 10:58:07 -0500

    :sparkles: Observable

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({ providedIn: 'root' })
+export class ServiceNameService {
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+  constructor(private httpClient: HttpClient) {}
+
+  searchCapital(query: string): Observable<Country[]> {
+    return this.httpClient.get<Country[]>(`${this.apiUrl}/capital/${query}`);
+  }
+}
